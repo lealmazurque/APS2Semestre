d2f313c7ae16ca682ab5e5c022f1d69a238a2bb9
LucasLMazurque <lucasmazurque@gmail.com>
2018-11-10 22:05:09 -0200

    Critografia Transposta

--- Criptografia_Transposta.cpp
+++ Criptografia_Transposta.cpp
@@ -1,50 +1,123 @@
-#include "pch.h"
-#include <iostream>
 #include <stdio.h>
-#include <cstring>
-#include <string.h>
+
 #include <stdlib.h>
-#include <string.h>>
 
-int main()
-{
-	char texto_usuario[n], textocriptografado[tamanho_string];// é aqui que nós vamos armazenar a string do usuário
-	char letra_usuario; // = 'alguma letra' /*guarda a letra digitada pelo usuário
-	int // conta_letra = 0,
-		/*vai armazenar a quantidade de vezes que a letra especificada pelo usuário
-							aparece na string*/
-		linha, //laço para Matriz das linhas
-		coluna, //laço para Matriz das colunas
-		i=0,j=0,                  /irá controlar o laço for()/
-		tamanho_string = 0; /irá guardar o tamanho da string digitada pelo usuário/
-	char Matriz[20][20], //Essa Matriz vai armazenar o texto que o usuário digitou
-		MatrizInv[20][20]; // essa aqui vai ser a Matriz do tipo Inversa, onde ela transformará linha em coluna
-
-		printf("Quantos Caracteres vai conter sua Mensagem?")
-		scanf("%d", &n);
-
-  inicio-
-	printf("\nDigite uma frase de no maximo %d Caracteres.\n",n);
-	gets("%s",&texto_usuario);/*o gets() serve para guardar uma string de caracteres na variável especificada entre
-			 os parênteses*/
-	texto_usuario = texto_usuario.Trim(); // Função TRIM remove todo os espaços em branco do texto digitado. Não funcionando ainda sla pq
+#include <string.h>
 
 
 
-	tamanho_string = strlen(texto_usuario);/*a variável tamanho_string irá conter o tamanho da string =) */
+#define TAM 1024
 
-		for (linha = 0; linha < 20; linha++)
-		{
+#define CHAVE 3
 
-			for (coluna = 0; coluna < 20; coluna++)
-			{
+//acima de 51199 pode dar pau... pq? não sei
 
-				Matriz[linha][coluna] = strlen(texto_usuario,1);
+#define MAX_LINHAS 51199
+
+
+
+int main(){
+
+	FILE *entrada = fopen("texto.txt", "r");
+
+	FILE *saida   = fopen("saida.txt", "wb+");
+
+	int i, j, cont, ultimoI, k;
+
+
+
+	printf("Transpocicao Criptografar\n");
+
+
+
+	char buffer[TAM];
+
+	char linha[TAM];
+
+
+
+	int matriz[MAX_LINHAS][CHAVE];
+
+
+
+	if(entrada != NULL && saida != NULL){
+
+		cont = 0;
+
+		j = 0;
+
+		k = 0;
+
+
+
+		while(!feof(entrada)){
+
+			for(i = 0; i < TAM; i++) linha[i] = '\0'; // limpa o buffer
+
+			for(i = 0; i < TAM; i++) buffer[i] = '\0'; // limpa o buffer
+
+
+
+			cont = fread(linha, sizeof(char), TAM, entrada);
+
+		}
+
+
+
+		for(i = 0, j = 0, k = 0; k < cont; ){
+
+			matriz[i][j] = linha[k];
+
+			k++;
+
+			j++;
+
+			if(j == CHAVE){
+
+				j = 0;
+
+				i++;
 
 			}
 
+			ultimoI = i;
+
+		}
 
 
 
-	   }
+		if(j < CHAVE){
+
+			for(cont = j; cont < CHAVE; cont++){
+
+				matriz[ultimoI][cont] = 55;
+
+			}
+
+		}
+
+
+
+		for(j = 0; j < CHAVE; j++){
+
+			for(i = 0; i <= ultimoI; i++){
+
+				buffer[0] = matriz[i][j];
+
+				fwrite(buffer, sizeof(char), 1, saida);
+
+			}
+
+		}
+
+	}
+
+
+
+	fclose(entrada);
+
+	fclose(saida);
+
+	return 0;
+
 }
